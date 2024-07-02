a5d8fdf2af5c2cb7dc64bb32b102595fedd9f315
Aman Singh <101966595+amansingh88688@users.noreply.github.com>
2024-07-02 20:09:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// onload = () => {
+//     const c = setTimeout(() => {
+//       document.body.classList.remove("not-loaded");
+//       clearTimeout(c);
+//     }, 1000);
+//   };
+
+
+function showNotLoadedAndPlayAudio() {
+  // Show not-loaded class after 1 second
+  const timeoutId = setTimeout(() => {
+    document.querySelector('.not-loaded').classList.remove("not-loaded");
+    clearTimeout(timeoutId);
+  }, 1000);
+
+  // Play audio in a loop
+  const audio = document.getElementById("myAudio");
+  audio.loop = true; // Set the loop attribute to true
+  audio.play();
+}
